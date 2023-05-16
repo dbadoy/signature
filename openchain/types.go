cad8a7d43c92400fa69e08500131ca4ebef25c91
dbadoy4874 <dbadoy4874@gmail.com>
2023-05-16 11:53:51 +0900

    openchain: add API `stats`

--- openchain/types.go
+++ openchain/types.go
@@ -6,17 +6,64 @@ const (
 	Version = "v1/"
 )
 
-type SignatureResponse struct {
-	Ok     bool   `json:"ok"`
-	Result Result `json:"result"`
+/*
+[Example]
+{
+  "ok": true,
+  "result": {
+    "event": {},
+    "function": {
+      "0xa9059cbb": [
+        {
+          "name": "transfer(address,uint256)",
+          "filtered": false
+        }
+      ]
+    }
+  }
 }
+*/
+type (
+	SignatureResponse struct {
+		Ok     bool            `json:"ok"`
+		Result SignatureResult `json:"result"`
+	}
 
-type Result struct {
-	Event  map[string][]Item `json:"event,omitempty"`
-	Method map[string][]Item `json:"function,omitempty"`
-}
+	SignatureResult struct {
+		Event  map[string][]Item `json:"event"`
+		Method map[string][]Item `json:"function"`
+	}
 
-type Item struct {
-	Name     string `json:"name"`
-	Filtered bool   `json:"filtered"`
+	Item struct {
+		Name     string `json:"name"`
+		Filtered bool   `json:"filtered"`
+	}
+)
+
+/*
+[Example]
+{
+  "ok": true,
+  "result": {
+    "count": {
+      "event": 373210,
+      "function": 2366040
+    }
+  }
 }
+*/
+type (
+	StatsResponse struct {
+		Ok     bool        `json:"ok"`
+		Result CountResult `json:"result"`
+	}
+
+	CountResult struct {
+		Count Count `json:"count"`
+	}
+
+	Count struct {
+		Event  int `json:"event"`
+		Method int `json:"function"`
+	}
+)
