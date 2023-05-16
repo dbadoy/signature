91822285be34f681ae4114a4277083655b71ebf0
dbadoy4874 <dbadoy4874@gmail.com>
2023-05-16 11:58:52 +0900

    fourbytes: syntax improvements, no internal behavior changes

--- fourbytes/types.go
+++ fourbytes/types.go
@@ -6,17 +6,36 @@ const (
 	Version = "v1/"
 )
 
-type SignatureResponse struct {
-	Count    int             `json:"count"`
-	Next     *string         `json:"next"`
-	Previous *string         `json:"previous"`
-	Results  []SignatureInfo `json:"results"`
+/*
+[Example]
+{
+    "count": 1,
+    "next": null,
+    "previous": null,
+    "results": [
+        {
+            "id": 238989,
+            "created_at": "2021-10-12T07:54:58.838466Z",
+            "text_signature": "getInitializationCodeFromContractRuntime_6CLUNS()",
+            "hex_signature": "0x00000009",
+            "bytes_signature": "\u0000\u0000\u0000\t"
+        }
+    ]
 }
+*/
+type (
+	SignatureResponse struct {
+		Count    int             `json:"count"`
+		Next     *string         `json:"next"`
+		Previous *string         `json:"previous"`
+		Results  []SignatureInfo `json:"results"`
+	}
 
-type SignatureInfo struct {
-	ID             uint64 `json:"id"`
-	CreatedAt      string `json:"created_at"`
-	TextSignature  string `json:"text_signature"`
-	HexSignature   string `json:"hex_signature"`
-	BytesSignature string `json:"bytes_signature"`
-}
+	SignatureInfo struct {
+		ID             uint64 `json:"id"`
+		CreatedAt      string `json:"created_at"`
+		TextSignature  string `json:"text_signature"`
+		HexSignature   string `json:"hex_signature"`
+		BytesSignature string `json:"bytes_signature"`
+	}
+)
